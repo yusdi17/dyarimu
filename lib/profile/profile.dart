8a4c6ca37aed611c1ea81001da2a434652940e6e
Hyemuuu <yosisofyan.p@gmail.com>
2024-07-06 07:16:43 +0700

    profile update Fixes #39

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:dyarimu/profile/chngepswd.dart';
+import 'dart:convert';
+import 'package:dyarimu/login_register/login.dart';
 import 'package:dyarimu/profile/editprofile.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyApp extends StatelessWidget {
   @override
@@ -18,8 +20,26 @@ class HalamanProfile extends StatefulWidget {
 }
 
 class _HalamanProfileState extends State<HalamanProfile> {
-  final String profilegambar =
-      'https://img.freepik.com/free-photo/portrait-smiling-young-businesswoman-with-hand-her-hips-showing-thumb-up-sign_23-2148087341.jpg?t=st=1718274320~exp=1718277920~hmac=e8e96ea9e5746013f782e413164d9eb76938321c1048e4cbbfd86356aebc7ac2&w=740';
+  late Future<Map<String, dynamic>> _profileData;
+
+  @override
+  void initState() {
+    super.initState();
+    _profileData =
+        fetchProfileData(); // Panggil fungsi untuk mengambil data profile
+  }
+
+  Future<Map<String, dynamic>> fetchProfileData() async {
+    var url = Uri.parse(
+        'https://932c-36-73-34-14.ngrok-free.app/profile/1'); // Ganti dengan URL endpoint profile Anda
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body); // Kembalikan data dalam bentuk Map
+    } else {
+      throw Exception('Failed to load profile data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,118 +49,100 @@ class _HalamanProfileState extends State<HalamanProfile> {
         title: Text('Profile', style: TextStyle(color: Colors.white)),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 20),
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(profilegambar),
-          ),
-          SizedBox(height: 10),
-          Text(
-            '@Mydyarimu',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            'Yosihayukk@gmail.com',
-            style: TextStyle(fontSize: 16, color: Colors.grey),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    '12',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Mengikuti',
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
-                  ),
-                ],
-              ),
-              SizedBox(width: 20),
-              Column(
-                children: [
-                  Text(
-                    '7',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Pengikut',
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Text(
-            'ini adalah bio',
-            style: TextStyle(fontSize: 16, color: Colors.grey),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                ElevatedButton(
-                  child: Text(
-                    'Edit Profile',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[300],
-                    minimumSize: Size(500, 45),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditProfile(),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _profileData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData) {
+            return Center(child: Text('No data available'));
+          } else {
+            var profile = snapshot.data!;
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${profile['nama_depan']} ${profile['nama_belakang']}',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      profile['bio'],
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      child: Text(
+                        'Edit Profile',
+                        style: TextStyle(color: Colors.black),
                       ),
-                    );
-                  },
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  child: Text(
-                    'Change Password',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[300],
-                    minimumSize: Size(500, 45),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChangePassword(),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[300],
+                        minimumSize: Size(500, 45),
                       ),
-                    );
-                  },
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditProfile(),
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      child: Text(
+                        'Sign Out',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[300],
+                        minimumSize: Size(500, 45),
+                      ),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => Login(),
+                                        ));
+                                  },
+                                  child: Text("Keluar")),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              HalamanProfile(),
+                                        ));
+                                  },
+                                  child: Text("batalkan")),
+                            ],
+                            title: Text("Sign out"),
+                            contentPadding: EdgeInsets.all(15),
+                            content: Text("Apakah Anda yakin ingin keluar?"),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  child: Text(
-                    'Sign Out',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey[300],
-                    minimumSize: Size(500, 45),
-                  ),
-                  onPressed: () {
-                    //fungsi disini
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
